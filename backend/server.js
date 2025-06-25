43f319a4951a3e026ad5021112c3ceba707ceab5
idtsitsharsh <harshg.prsnl@gmail.com>
2025-06-25 05:33:44 +0530

    Some Changes

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 const frontendURL = process.env.FRONTEND_URL;
 const allowedOrigins = [
   'http://localhost:3000', 
-  frontendURL,
+  frontendURL
 ];
 
 if (frontendURL) {
